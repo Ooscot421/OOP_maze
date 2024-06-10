5ec8dbfbc558f5d59bd14f51976735c44cb858b0
王子儀 <115283988+Allenwang2004@users.noreply.github.com>
2024-06-11 01:49:04 +0800

    Update main.cpp Allen0611

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include "maze.h"
 #include "player.h"
+#include "block.h"
 using namespace std;
 
 int main(int argc, char** argv){
@@ -22,12 +23,37 @@ int main(int argc, char** argv){
         while(true) {   //Condition for ending the game of this level. Write your own function to replace "true"
             char direction = player->get_direction();
             cout << "Main->direction = " << direction << endl;
-
-
             //Codes for the player touching different types of block. Use the function player_touched() to implement
-
-            player->move(direction, maze);
-
+            int nextRow = player->get_player_row(), nextCol = player->get_player_col();
+            switch (direction) {
+                case 'U':
+                    nextRow -= 1;
+                    break;
+                case 'D':
+                    nextRow += 1;
+                    break;
+                case 'L':
+                    nextCol -= 1;
+                    break;
+                case 'R':
+                    nextCol += 1;
+                    break;
+                default:
+                    return 0;
+            }
+            if (nextRow >= 0 && nextRow < maze->get_nRow() && nextCol >= 0 && nextCol < maze->get_nCol()) 
+            {
+                Block* nextBlock = maze->get_block(nextRow, nextCol);
+                if (nextBlock->get_type() == tWall) {
+                    nextBlock->player_touched();
+                }
+                else if (nextBlock->get_type() == tGoal) {
+                    break;
+                }
+                else {
+                    player->move(direction, maze);
+                }
+            }
             maze->printMaze(player);
         }
 
